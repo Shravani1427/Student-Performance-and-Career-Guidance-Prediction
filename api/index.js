b18dbd6d135a47b758595e211b8ade24780893c9
Shravani Chavan <shravanichavan779@gmail.com>
2026-08-14 16:54:39 +0530

    Fix Vercel API

--- api/index.js
+++ api/index.js
@@ -1,12 +1,5 @@
 "use strict";
 
-/*
-=========================================================
-VERCEL SERVERLESS API ENTRY POINT
-Student Performance & Career Guidance System
-=========================================================
-*/
-
 const app = require("../server");
 
 module.exports = app;
